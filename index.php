9c3104961fbe12a341f02ef0de331fb03f34c13a
nolive <nolive@gmx.fr>
2023-07-26 18:20:28 +0200

    modified:   css/master.css

--- index.php
+++ index.php
@@ -22,17 +22,11 @@ include './inc/multilang.php';
     <div class="main-container d-flex f-wrap g-1">
         <?php include("./aside.php") ?>
         <main class="p-1 bgm">
-            <div class="text-center" style="min-height : 20%; padding-top : 12%">
+            <div class="text-center titreh1">
                 <h1><?php echo $lang['HEADER_TITLE']; ?></h1>
-               
-                    <div class="inline-block">
-                        <div class="type1">I'm a full stack Web developper ! </div>
-                    </div>
-                
-
-
-
-
+                <div class="inline-block">
+                    <div class="type1 type2">I'm a full stack Web developper ! </div>
+                </div>
             </div>
             <p>Ex ad laborum enim deserunt excepteur nulla duis excepteur adipisicing deserunt fugiat in.</p>
             <div class="d-flex g-1">
