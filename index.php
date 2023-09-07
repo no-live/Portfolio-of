080efcbe7bf4f3c0d8d5c36b65ae702d6d5d3dc4
nolive <nolive@gmx.fr>
2023-09-07 18:10:39 +0200

    modified:   index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ include './inc/multilang.php';
 ?>
 <!DOCTYPE html>
 
-<html lang="<?php echo $lang['LANG'];?>">
+<html lang="<?php echo $lang['LANG']; ?>">
 
 
 <head>
@@ -22,11 +22,60 @@ include './inc/multilang.php';
     <div class="main-container d-flex f-wrap g-1">
         <?php include("./aside.php") ?>
         <main class="p-1 bgm">
-            <div class="text-center titreh1">
-                <h1><?php echo $lang['HEADER_TITLE']; ?></h1>
-            </div>
+            <?php
+            if (!isset($_SESSION['lang']) or ($_SESSION['lang']) == 'FR') {
+            ?>
+                
+                    <h1 class="d-flex f-dir-row f-wrap jc-c title1">
+                        <p class="bouncing-letters text-center">
+                            <span>B</span><span>o</span><span>n</span><span>j</span><span>o</span><span>u</span><span>r</span><span>!</span>&nbsp
+                        </p>
+                        <p class="bouncing-letters text-center">
+                            <span>D</span><span>é</span><span>v</span><span>e</span><span>l</span><span>o</span><span>p</span><span>p</span><span>e</span><span>u</span><span>r</span>
+                           
+                            <span>W</span><span>e</span><span>b</span><span>/</span>&nbsp
+                        </p>
+                        <p class="bouncing-letters text-center">
+                            <span>W</span><span>e</span><span>b</span>
+                            
+                            <span>m</span><span>o</span><span>b</span><span>i</span><span>l</span><span>e</span>&nbsp
+                        </p>
+                        <p class="bouncing-letters text-center">
+                            <span>F</span><span>u</span><span>l</span><span>l</span><span>s</span><span>t</span><span>a</span><span>c</span><span>k</span>
+                        </p>
+
+                    </h1>
+
+                
+            <?php } else {
+            ?>
+                
+                    <h1 class="d-flex f-dir-row f-wrap jc-c title1">
+                        <p class="bouncing-letters">
+                            <span>H</span><span>e</span><span>l</span><span>l</span><span>o</span><span>!</span>&nbsp
+                        </p>
+                        <p class="bouncing-letters">
+                            <span>F</span><span>u</span><span>l</span><span>l</span><span>s</span><span>t</span><span>a</span><span>c</span><span>k</span>
+                            <!-- <div class="bouncing-letters"> -->
+                            <span>W</span><span>e</span><span>b</span><span>/</span>&nbsp
+                        </p>
+                        <p class="bouncing-letters">
+                            <span>M</span><span>o</span><span>b</span><span>i</span><span>l</span>
+                            <!-- <div class="bouncing-letters"> -->
+                            <span>W</span><span>e</span><span>b</span>&nbsp
+                        </p>
+                        <p class="bouncing-letters">
+                            <span>D</span><span>e</span><span>v</span><span>e</span><span>l</span><span>o</span><span>p</span><span>e</span><span>r</span>
+                        </p>
+
+                    </h1>
+
+                
+            <?php } ?>
             <div class="inline-block rounded">
-                <div class="type1"><span1></span1></div>
+                <div class="type1">
+                    <span1></span1>
+                </div>
             </div>
             <p>Ex ad laborum enim deserunt excepteur nulla duis excepteur adipisicing deserunt fugiat in.</p>
             <div class="d-flex g-1">
---
+++ js/bouncing.js
@@ -0,0 +1,16 @@
+console.log("script bouncing.js is running...");
+document.querySelectorAll(".bouncing-letters>span").forEach((element) => {
+    element.addEventListener("mouseover", (e) => bounce(e.target));
+});
+
+function bounce(letter) {
+    if (!letter.classList.contains("bounce")) {
+        letter.classList.add("bounce");
+        setTimeout(
+            function () {
+                letter.classList.remove("bounce");
+            },
+            1000
+        );
+    }
+}
